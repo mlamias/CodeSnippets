165718b7a866ee1ea6f526aeb76a4d2783974e1d
Mark J. Lamias <mlamias@yahoo.com>
2020-11-19 00:37:06 -0500

    Initial Upload of GetPostalAbbreviations.R

---
+++ R/GetPostalAbbreviations.R
@@ -0,0 +1,23 @@
+# ****************************************************************************
+# Title       : GetPostalAbbreviations.R
+# Description : This program obtains a current list of postal abbreviations
+#             : and state/territory full names from the US Postal Service
+#             :  
+# Author      : Mark J. Lamias, The Stochastic Group, Inc.
+# Date        : 11/19/2020
+# Email       : mlamias at stochasticgroup.com
+# Outputs     : tibble named state_abbreviations
+# Example Use : getStates()
+# More info   : https://pe.usps.com/text/pub28/28apb.htm
+#
+# Copyright (c) Mark J. Lamias, 2020
+# ****************************************************************************
+
+library(rvest)
+functions<-getStates(){
+URL<-"https://pe.usps.com/text/pub28/28apb.htm"
+abbreviations <- read_html(URL) %>% 
+  html_nodes("table") %>% 
+  `[[`(4) %>% 
+  html_table() %>% rename(state_name=1, state_abbreviation = 2)
+}
